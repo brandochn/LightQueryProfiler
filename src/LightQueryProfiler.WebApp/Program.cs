00557f1b04c0d283bfb0261211136ec4598c1c27
Hildebrando Chavez <brandochn@gmail.com>
2022-09-10 08:16:51 -0500

    Add chromely component

--- src/LightQueryProfiler.WebApp/Program.cs
+++ src/LightQueryProfiler.WebApp/Program.cs
@@ -1,27 +1,85 @@
-using LightQueryProfiler.SharedWebUI.Data;
-using LightQueryProfiler.WebApp.Data;
+/*
+ Code adapted from:
+https://github.com/chromelyapps/demo-projects/tree/master/blazor
+September 2022
+ */
 
-var builder = WebApplication.CreateBuilder(args);
+using Chromely;
+using Chromely.Core;
+using Chromely.Core.Configuration;
+using LightQueryProfiler.Shared.Util;
+using LightQueryProfiler.WebApp;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddScoped<LightQueryProfilerInterop>();
+string tempPath = ServerAppUtil.CreateTempFile(Path.GetTempPath());
+bool firstProcess = ServerAppUtil.IsMainProcess(args);
+int port = ServerAppUtil.AvailablePort;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (firstProcess)
 {
-    app.UseExceptionHandler("/Error");
+    if (port != -1)
+    {
+        // start the kestrel server in a background thread
+        AppDomain.CurrentDomain.ProcessExit += ServerAppUtil.ProcessExit;
+        ServerAppUtil.BlazorTaskTokenSource = new CancellationTokenSource();
+        ServerAppUtil.BlazorTask = new Task(() =>
+        {
+            BlazorAppBuilder.Create(args, port)
+                .Build()
+                .Run();
+        }, ServerAppUtil.BlazorTaskTokenSource.Token, TaskCreationOptions.LongRunning);
+        ServerAppUtil.BlazorTask.Start();
+
+        // wait till its up
+        while (ServerAppUtil.IsPortAvailable(port))
+        {
+            Thread.Sleep(1);
+        }
+    }
+
+    // Save port for later use by chromely processes
+    ServerAppUtil.SavePort(tempPath, port);
+}
+else
+{
+    // fetch port number
+    port = ServerAppUtil.GetSavedPort(tempPath);
 }
 
-app.UseStaticFiles();
+if (port != -1)
+{
+    // start up chromely
+    var config = DefaultConfiguration.CreateForRuntimePlatform();
+    config.WindowOptions.Title = "Light Query Profiler";
+    config.StartUrl = $"https://127.0.0.1:{port}";
+#if DEBUG
+    // show/hide chrome dev tools
+    config.DebuggingMode = true;
+#else
+    config.DebuggingMode = false;
+#endif
 
-app.UseRouting();
+    config.WindowOptions.WindowState = Chromely.Core.Host.WindowState.Maximize;
+    //config.WindowOptions.RelativePathToIconFile = "chromely.ico";
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+    try
+    {
+        var builder = AppBuilder.Create(args);
+        builder = builder.UseConfig<DefaultConfiguration>(config);
+        builder = builder.UseApp<LightQueryProfilerApp>();
+        builder = builder.Build();
+        builder.Run();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+        throw;
+    }
+}
 
-app.Run();
+public class LightQueryProfilerApp : ChromelyBasicApp
+{
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        base.ConfigureServices(services);
+    }
+}
