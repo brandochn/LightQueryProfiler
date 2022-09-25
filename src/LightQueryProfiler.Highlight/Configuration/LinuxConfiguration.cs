0025259343c610ff6453d629bdf8f9de2b8c852e
Hildebrando Chavez <brandochn@gmail.com>
2022-09-25 18:10:48 -0500

    Fix an issue with SQL Highlight functionality needs a specific font for each OS

---
+++ src/LightQueryProfiler.Highlight/Configuration/LinuxConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+
+namespace LightQueryProfiler.Highlight.Configuration
+{
+    public class LinuxConfiguration : XmlConfiguration
+    {
+        public LinuxConfiguration()
+        {
+            XmlDocument = XDocument.Parse(Resources.LinuxDefinitions);
+        }
+    }
+}
