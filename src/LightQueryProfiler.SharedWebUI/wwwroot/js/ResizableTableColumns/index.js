fe9b963f1fea62b630fccf708acae9502d540910
Hildebrando Chavez <32686301+brandochn@users.noreply.github.com>
2024-03-20 18:46:12 -0600

    Remove unnecessary web projects. (#21)

--- src/LightQueryProfiler.SharedWebUI/wwwroot/js/ResizableTableColumns/index.js
+++
@@ -1,7 +0,0 @@
-export * from './resizable-constants.js';
-export * from './resizable-event-data.js';
-export * from './resizable-options.js';
-export * from './resizable-table-columns.js';
-export * from './utilities-dom.js';
-export * from './utilities.js';
-//# sourceMappingURL=index.js.map
